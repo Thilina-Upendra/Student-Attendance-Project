cf0292fabc5366a14ac260728f93fe6bbda0d349
Thilina-Upendra <upendravass1996@gmail.com>
2022-01-26 11:52:08 +0530

    Design ImportDBForm

---
+++ src/controller/ImportDBFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ImportDBFormController {
+
+    public RadioButton rdoRestore;
+    public ToggleGroup grouped;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public RadioButton rdoFirstTime;
+    public Button btnOkay;
+}
