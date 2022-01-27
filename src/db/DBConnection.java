0169898fabdc6ba8726f0b87b8857f6f70bb4b47
Thilina-Upendra <upendravass1996@gmail.com>
2022-01-27 11:35:11 +0530

    Create DBConnection to the project.

---
+++ src/db/DBConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+
+    private DBConnection(){
+
+    }
+
+    public void init(Connection connection){
+        if(this.connection==null){
+            this.connection = connection;
+        }else if(this.connection != connection){
+            throw new RuntimeException("The connection has been already initialized..");
+        }
+    }
+
+    public static DBConnection getInstance(){
+        return dbConnection==null? dbConnection = new DBConnection(): dbConnection;
+    }
+
+    public Connection getConnection(){
+        if(connection==null){
+            throw new RuntimeException("Init the connection first..");
+        }
+        return connection;
+    }
+}
